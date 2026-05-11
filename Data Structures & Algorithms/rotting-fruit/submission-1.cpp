4cf0730e0172787c5e7e3a0785b8f7982e51565e
Rahul singh <77066215+Rahulsingh0106@users.noreply.github.com>
2026-05-11 07:34:08 +0530

    Add: rotting-fruit - submission-1

---
+++ Data Structures & Algorithms/rotting-fruit/submission-1.cpp
@@ -0,0 +1,144 @@
+class Solution {
+public:
+    /*int orangesRotting(vector<vector<int>>& grid) {
+
+        int rows = grid.size();
+        int cols = grid[0].size();
+
+        queue<pair<int,int>> q;
+
+        int fresh = 0;
+
+        // Step 1:
+        // Find all rotten oranges
+        // Count fresh oranges
+        for(int row = 0; row < rows; row++) {
+
+            for(int col = 0; col < cols; col++) {
+
+                if(grid[row][col] == 2) {
+                    q.push({row, col});
+                }
+
+                else if(grid[row][col] == 1) {
+                    fresh++;
+                }
+            }
+        }
+
+        int minutes = 0;
+
+        // directions
+        vector<pair<int,int>> directions = {
+            {-1,0}, // up
+            {1,0},  // down
+            {0,-1}, // left
+            {0,1}   // right
+        };
+
+        // BFS
+        while(!q.empty() && fresh > 0) {
+
+            int size = q.size();
+
+            // process one BFS level
+            for(int i = 0; i < size; i++) {
+
+                auto current = q.front();
+                q.pop();
+
+                int row = current.first;
+                int col = current.second;
+
+                // explore neighbors
+                for(auto dir : directions) {
+
+                    int newRow = row + dir.first;
+                    int newCol = col + dir.second;
+
+                    // boundary check
+                    if(newRow >= 0 && newRow < rows &&
+                       newCol >= 0 && newCol < cols &&
+
+                       // fresh orange check
+                       grid[newRow][newCol] == 1) {
+
+                        // make rotten
+                        grid[newRow][newCol] = 2;
+
+                        // reduce fresh count
+                        fresh--;
+
+                        // push into queue
+                        q.push({newRow, newCol});
+                    }
+                }
+            }
+
+            // one level completed
+            minutes++;
+        }
+
+        // still fresh oranges left
+        if(fresh > 0) {
+            return -1;
+        }
+
+        return minutes;
+    }*/
+
+    int orangesRotting(vector<vector<int>>& grid){
+        int rows = grid.size();
+        int cols = grid[0].size();
+
+        queue<pair<int, int>> q;
+        int minutes = 0;
+        int fresh = 0;
+
+        for(int row = 0; row < rows; row++){
+            for(int col = 0; col < cols; col++){
+                if(grid[row][col] == 1){
+                    fresh++;
+                }
+
+                else if(grid[row][col] == 2){
+                    q.push({row, col});
+                }
+            }
+        }
+
+        vector<pair<int, int>> directions = {
+            {-1, 0},
+            {1, 0},
+            {0, -1},
+            {0, 1}
+        };
+
+        while(!q.empty() && fresh > 0){
+            int size = q.size();
+            
+            for(int i = 0; i < size; i++){
+                auto current = q.front();
+                q.pop();
+                int row = current.first;
+                int col = current.second;
+
+                for(auto dir : directions){
+                    int newRow = row + dir.first;
+                    int newCol = col + dir.second;
+
+                    if(newRow < rows && newRow >= 0 && newCol >= 0 && newCol < cols && grid[newRow][newCol] == 1){
+                        fresh--;
+                        grid[newRow][newCol] = 2;
+                        q.push({newRow, newCol});
+                    }
+                }
+            }
+            minutes++;
+        } 
+        if(fresh > 0){
+            return -1;
+        }
+        return minutes;  
+    }
+};
