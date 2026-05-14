2107c9c2d30fdb6f686536dd035408d1f16485bc
Rahul singh <77066215+Rahulsingh0106@users.noreply.github.com>
2026-05-14 07:27:14 +0530

    Add: invert-a-binary-tree - submission-3

---
+++ Data Structures & Algorithms/invert-a-binary-tree/submission-3.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(root == NULL){
+            return root;
+        }
+        
+        TreeNode* left = root->left;
+
+        root->left = root->right;
+        root->right = left;
+
+        invertTree(root->left);
+        invertTree(root->right);
+        return root;
+    }
+};
